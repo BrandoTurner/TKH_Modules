c679edcff5986d4f3ad99e8cd64c0c9572b2d2a6
Brandon Turner <84806464+BrandoTurner@users.noreply.github.com>
2021-08-19 16:54:41 -0400

    cleanup

--- Module_5_Programming1/Assignment1.js
+++ Module_5_Programming1/Assignment1.js
@@ -10,10 +10,10 @@ let yearlySavings = parseInt(prompt("How much do you to save in a year?"))
 	let weeklySavings  = yearlySavings / 52
 	let amntToSave = parseInt(weeklySavings.toFixed(2);
 
-	let totalCost = foodCost + housingCost + transportationCost + otherCost
-	let revenue = weeklyIncome - totalCost
+		let totalCost = foodCost + housingCost + transportationCost + otherCost
+		let revenue = weeklyIncome - totalCost
 
-		if( revenue > amntToSave){
+			if( revenue > amntToSave){
 			console.log("You're on the right track!")
 			}
 
